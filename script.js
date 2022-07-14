40acd97463cf821d0e451c50bf0c222cec1ba26b
phillipe henriques <phillipe_ch@hotmail.com>
2022-07-14 19:47:28 -0300

    dando sequência na função

---
+++ script.js
@@ -0,0 +1,20 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Digite um número válido')
+    } else {
+        var fsex = document.getElementsByTagName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+    }
+    
+}
